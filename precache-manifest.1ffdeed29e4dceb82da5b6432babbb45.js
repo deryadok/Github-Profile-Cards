9dc7e786b5014c21d4703b488600f5db740914b1
Derya Dok <derya.dok@ogr.deu.edu.tr>
2020-01-16 13:31:17 +0300

    Updates

---
+++ precache-manifest.1ffdeed29e4dceb82da5b6432babbb45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f0c0e68f72f39527a68e8c05808ea6",
+    "url": "/Github-Profile-Cards/index.html"
+  },
+  {
+    "revision": "660b242526f7bdd09c40",
+    "url": "/Github-Profile-Cards/static/css/main.c167abeb.chunk.css"
+  },
+  {
+    "revision": "7cffcf7630fbbbc44989",
+    "url": "/Github-Profile-Cards/static/js/2.0e9338d8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Github-Profile-Cards/static/js/2.0e9338d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "660b242526f7bdd09c40",
+    "url": "/Github-Profile-Cards/static/js/main.97273df2.chunk.js"
+  },
+  {
+    "revision": "11691146b09aa62cf814",
+    "url": "/Github-Profile-Cards/static/js/runtime-main.4c39c992.js"
+  }
+]);
